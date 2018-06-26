b60119d5b238a8b68165fdff365392d873b40d75
Farhad <ifarhadfaghihi@gmail.com>
2018-06-26 17:48:11 +0430

    added operations tests

--- src/main/java/io/github/farhad/rpn/Calculator.java
+++ src/main/java/io/github/farhad/rpn/Calculator.java
@@ -22,4 +22,32 @@ public class Calculator {
     public void drop(){
         stack.pop();
     }
+
+    public void add() {
+        BigDecimal firstValue = stack.peek();
+        stack.pop();
+        BigDecimal secondValue = stack.peek();
+        stack.replaceTop(firstValue.add(secondValue));
+    }
+
+    public void subtract() {
+        BigDecimal firstValue = stack.peek();
+        stack.pop();
+        BigDecimal secondValue = stack.peek();
+        stack.replaceTop(secondValue.subtract(firstValue));
+    }
+
+    public void multiply() {
+        BigDecimal firstValue = stack.peek();
+        stack.pop();
+        BigDecimal secondValue = stack.peek();
+        stack.replaceTop(secondValue.multiply(firstValue));
+    }
+
+    public void divide() {
+        BigDecimal firstValue = stack.peek();
+        stack.pop();
+        BigDecimal secondValue = stack.peek();
+        stack.replaceTop(secondValue.divide(firstValue,5, BigDecimal.ROUND_HALF_EVEN));
+    }
 }
---
+++ src/test/java/io.github.farhad.rpn/OperationTest.java
@@ -0,0 +1,52 @@
+package io.github.farhad.rpn;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class OperationTest {
+
+    private Calculator calculator ;
+
+    @Before
+    public void setUp(){
+        calculator = new Calculator();
+        calculator.setAccumulator(new BigDecimal(5));
+        calculator.enter();
+        calculator.setAccumulator(new BigDecimal(15));
+    }
+
+    @Test
+    public void testAdd(){
+        calculator.add();
+        Assert.assertEquals(BigDecimal.valueOf(20),calculator.getAccumulator());
+    }
+
+    @Test
+    public void testSubtract(){
+        calculator.subtract();
+        Assert.assertEquals(BigDecimal.valueOf(-10),calculator.getAccumulator());
+    }
+
+    @Test
+    public void testMultiply(){
+        calculator.multiply();
+        Assert.assertEquals(BigDecimal.valueOf(75),calculator.getAccumulator());
+    }
+
+    @Test
+    public void testDivide(){
+        calculator.divide();
+        BigDecimal expected = BigDecimal.valueOf(0.33333d).setScale(5,BigDecimal.ROUND_HALF_EVEN) ;
+        Assert.assertEquals(expected,calculator.getAccumulator());
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+}
