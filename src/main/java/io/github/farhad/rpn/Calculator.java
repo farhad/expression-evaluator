400d220a7cec780633056144963fbde3a0511a83
Farhad <ifarhadfaghihi@gmail.com>
2018-06-26 16:36:09 +0430

    refactored the calculator design

--- src/main/java/io/github/farhad/rpn/Calculator.java
+++ src/main/java/io/github/farhad/rpn/Calculator.java
@@ -5,25 +5,21 @@ import java.util.Stack;
 
 public class Calculator {
 
-    private Stack<BigDecimal> values = new Stack<>();
+    private OperandStack stack = new OperandStack() ;
 
     public BigDecimal getAccumulator() {
-        if(values.size() == 0)
-            return BigDecimal.ZERO ;
-        return values.peek();
+       return stack.peek();
     }
 
     public void setAccumulator(BigDecimal value) {
-        if(values.size() > 0)
-            values.pop();
-        values.push(value);
+        stack.replaceTop(value);
     }
 
     public void enter() {
-        values.push(getAccumulator());
+        stack.push(getAccumulator());
     }
 
     public void drop(){
-        values.pop();
+        stack.pop();
     }
 }
